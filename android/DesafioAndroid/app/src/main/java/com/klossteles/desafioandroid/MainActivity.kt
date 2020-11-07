f79c0a2914b842b399f9eb6468e43b0894ec8eb7
klossteles <klossteles@gmail.com>
2020-11-07 17:00:49 -0300

    Add animation to navigation

--- android/DesafioAndroid/app/src/main/java/com/klossteles/desafioandroid/MainActivity.kt
+++ android/DesafioAndroid/app/src/main/java/com/klossteles/desafioandroid/MainActivity.kt
@@ -2,24 +2,20 @@ package com.klossteles.desafioandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        navController.navigateUp()
-        return super.onSupportNavigateUp()
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
