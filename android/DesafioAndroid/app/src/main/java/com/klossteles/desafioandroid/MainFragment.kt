cd0330510db9f9466c754d87397a9e9875eebb0b
klossteles <klossteles@gmail.com>
2020-11-07 16:42:48 -0300

    Add register fragment and navigation

---
+++ android/DesafioAndroid/app/src/main/java/com/klossteles/desafioandroid/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.klossteles.desafioandroid
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+
+        view.findViewById<Button>(R.id.btnLogin).setOnClickListener {
+
+        }
+
+        view.findViewById<Button>(R.id.btnRegister).setOnClickListener {
+            navController.navigate(R.id.registerFragment)
+        }
+    }
+
+}
