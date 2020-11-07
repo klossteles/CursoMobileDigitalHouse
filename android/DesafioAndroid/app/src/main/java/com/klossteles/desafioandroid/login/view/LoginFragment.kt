904626b03c466ef75454e3c7e2634c802cac89de
klossteles <klossteles@gmail.com>
2020-11-07 18:01:53 -0300

    Add login validations

--- android/DesafioAndroid/app/src/main/java/com/klossteles/desafioandroid/login/view/LoginFragment.kt
+++ android/DesafioAndroid/app/src/main/java/com/klossteles/desafioandroid/login/view/LoginFragment.kt
@@ -1,12 +1,17 @@
 package com.klossteles.desafioandroid.login.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.klossteles.desafioandroid.R
 
 class LoginFragment : Fragment() {
@@ -22,13 +27,55 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val navController = findNavController()
 
+        onLogin(view)
+        onRegister(view, navController)
+        checkEmailChanged(view)
+    }
+
+    private fun onLogin(view: View) {
         view.findViewById<Button>(R.id.btnLogin).setOnClickListener {
-
+            var success = true
+            val email = view.findViewById<TextInputEditText>(R.id.edtEmailLogin)?.text.toString()
+            if (email.isEmpty()) {
+                view.findViewById<TextInputLayout>(R.id.txtEmailLogin).error = getString(R.string.insert_email)
+                success = false
+            }
+            val password = view.findViewById<TextInputEditText>(R.id.edtPasswordLogin)?.text.toString()
+            success = checkPassword(password, view, success)
         }
+    }
 
+    private fun onRegister(view: View, navController: NavController) {
         view.findViewById<Button>(R.id.btnRegisterLogin).setOnClickListener {
             navController.navigate(R.id.action_loginFragment_to_registerFragment)
         }
     }
 
+    private fun checkEmailChanged(view: View) {
+        view.findViewById<TextInputEditText>(R.id.edtEmailLogin)
+            .addTextChangedListener(object :
+                TextWatcher {
+                override fun afterTextChanged(s: Editable?) { }
+
+                override fun beforeTextChanged( s: CharSequence?, start: Int, count: Int, after: Int ) { }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    view.findViewById<TextInputLayout>(R.id.txtEmailLogin).error = ""
+                }
+            })
+    }
+
+    private fun checkPassword(password: String, view: View, success: Boolean): Boolean {
+        var isOk = success
+        if (password.isEmpty()) {
+            view.findViewById<TextInputLayout>(R.id.txtPasswordLogin).error =
+                getString(R.string.password_cannot_be_null)
+            isOk = false
+        } else if (password.length < 6) {
+            view.findViewById<TextInputLayout>(R.id.txtPasswordLogin).error =
+                getString(R.string.must_contain_at_least_6_characters)
+            isOk = false
+        }
+        return isOk
+    }
 }
