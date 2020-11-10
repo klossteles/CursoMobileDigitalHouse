0c84c01a1eeaa158c6d2bdc17a595a8df5b5d885
klossteles <klossteles@gmail.com>
2020-11-10 18:52:30 -0300

    Add splashscreen

---
+++ android/DesafioAndroid/app/src/main/java/com/klossteles/desafioandroid/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.klossteles.desafioandroid
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.TextView
+
+const val SPLASH_DURATION = 4000L
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        putSplashFullSize()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DURATION)
+    }
+
+    private fun putSplashFullSize() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
